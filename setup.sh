a5cd3a5d088cfdf7746fc81b059b286052485774
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-10-04 06:41:41 +0300

    fix setup with full path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker build -t $DOCKER_REPO .
-docker run --detach --name $DOCKER_REPO --publish 6878:6878 --env HOME=. \
-    $DOCKER_REPO sh -c "./acestreamengine --client-console"
-
+docker run --detach --name $DOCKER_REPO --publish 6878:6878 \
+    $DOCKER_REPO /srv/acestream/start-engine --client-console
