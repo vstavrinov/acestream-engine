fd61ae0921f232c82a8852cb3052a51a75257e1e
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-10-04 18:27:23 +0300

    fix deploy (typo)

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ docker tag $DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:latest
 echo $DOCKER_PASSWORD |
 docker login -u $DOCKER_USERNAME --password-stdin
 docker push $DOCKER_USERNAME/$DOCKER_REPO:$TAG
-docker push $DOCKER_USERNAME/$DOCKER_REPO:$latest
+docker push $DOCKER_USERNAME/$DOCKER_REPO:latest
 curl -s -X POST                                 \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token $TOKEN"            \
