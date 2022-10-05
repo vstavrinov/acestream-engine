70a74c082673cc58fab7b8666fc3d1399e3322a4
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-10-05 03:20:20 +0300

    move REST request from script to workflow yaml

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,3 @@ echo $DOCKER_PASSWORD |
 docker login -u $DOCKER_USERNAME --password-stdin
 docker push $DOCKER_USERNAME/$DOCKER_REPO:$TAG
 docker push $DOCKER_USERNAME/$DOCKER_REPO:latest
-REST="curl -siX POST                            \
-    -H 'Accept: application/vnd.github.v3+json' \
-    -H 'Authorization: Bearer $TOKEN'           \
-    -d '{"ref": "master"}'                      \
-    https://api.github.com/repos/$ENDPOINT/dispatches"
-
-RETURN="$(eval "$REST")"
-echo "$RETURN"
-STATUS=$(echo "$ETURN" |
-    head -1            |
-    awk '{print $2}')
-
-if [ ${STATUS:=400} -ge 400 ]; then
-    set -x -v
-    eval "$REST"
-    exit 1
-fi
