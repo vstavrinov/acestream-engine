75dc7a4bbf588b4dda61da68745f8ace671a687a
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-11-18 20:15:52 +0300

    fix path to start engine

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 docker build -t $DOCKER_REPO .
 docker run --detach --name $DOCKER_REPO --publish 6878:6878 \
-    $DOCKER_REPO /srv/acestream/start-engine --client-console
+    $DOCKER_REPO /srv/ace/start-engine --client-console
 
 sleep 2
