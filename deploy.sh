f44068f917dccc0b3b9704895d470f3e889c41b4
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2024-06-02 17:10:21 +0300

    deploy to github repository

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,12 @@ echo $DOCKER_PASSWORD |
 docker login -u $DOCKER_USERNAME --password-stdin
 docker push $DOCKER_USERNAME/$DOCKER_REPO:$TAG
 docker push $DOCKER_USERNAME/$DOCKER_REPO
+echo $GITHUB_PASSWORD |
+docker login -u $GITHUB_USERNAME --password-stdin $GITHUB_REGISTRY
+docker tag $DOCKER_USERNAME/$DOCKER_REPO $GITHUB_REGISTRY/$IMAGE_NAME:$TAG
+docker tag $DOCKER_USERNAME/$DOCKER_REPO $GITHUB_REGISTRY/$IMAGE_NAME:latest
+docker push $GITHUB_REGISTRY/$IMAGE_NAME:$TAG
+docker push $GITHUB_REGISTRY/$IMAGE_NAME:latest
 
 COMMIT=$(git rev-parse --short $GITHUB_SHA)
 REST="curl -siX POST                            \
